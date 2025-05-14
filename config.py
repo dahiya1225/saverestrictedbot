92a64c139375d111e4229cf6a739a8e47eeea638
Shubham Dahiya <ekjaatpoorasmart@gmail.com>
2025-05-14 08:51:24 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,30 @@
+# devgagan
+# Note if you are trying to deploy on vps then directly fill values in ("")
+
+from os import getenv
+
+# VPS --- FILL COOKIES 🍪 in """ ... """ 
+
+INST_COOKIES = """
+# wtite up here insta cookies
+"""
+
+YTUB_COOKIES = """
+# write here yt cookies
+"""
+
+API_ID = int(getenv("25894883", ""))
+API_HASH = getenv("515876c537d9329ff9c91 e52da58c6ca", "")
+BOT_TOKEN = getenv("7714902342:AAGb_Gdj9lbuZjxwUgy6AlR3wBMWGwKO54w", "")
+OWNER_ID = list(map(int, getenv("@sunil002", "").split()))
+MONGO_DB = getenv("MONGO_DB", "")
+LOG_GROUP = getenv("LOG_GROUP", "")
+CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
+WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
+AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
+STRING = getenv("STRING", None)
+YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
+DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
+INSTA_COOKIES = getenv("INSTA_COOKIES", INST_COOKIES)
