e20840a77c21a5b7d4801c949118c7ebace308f0
Shubham Dahiya <ekjaatpoorasmart@gmail.com>
2025-05-15 16:49:50 +0530

    Initial commit

--- config.py
+++ config.py
@@ -14,9 +14,12 @@ YTUB_COOKIES = """
 """
 
 API_ID = int(getenv("API_ID", "25894883"))
-API_HASH = getenv("515876c537d9329ff9c91 e52da58c6ca", "")
-BOT_TOKEN = getenv("7714902342:AAGb_Gdj9lbuZjxwUgy6AlR3wBMWGwKO54w", "")
-
+API_HASH = getenv("API_HASH", "515876c537d9329ff9c91 e52da58c6ca")
+BOT_TOKEN = getenv("BOT_TOKEN", "7714902342:AAGb_Gdj9lbuZjxwUgy6AlR3wBMWGwKO54w")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8117399125").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://ekjaatpoorasmart:<db_password>@cluster0.uyq1wig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002572567636")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002572567636"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
